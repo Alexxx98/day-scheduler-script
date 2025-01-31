630a81f6352bacc014d50e4713e8fd382ae55a7d
Aleksander Pres <aleksander.pres98@gmail.com>
2025-01-31 19:04:48 +0100

    Add desktop notifications

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ jobstore = {
 async def send_task_message(user_id: int, message: str):
     user = bot.get_user(user_id)
     await user.send(message)
-    await plyer.notification.notify(
+    plyer.notification.notify(
         title="Day Scheduler",
         message=message,
         app_icon="bot-icon.png",
